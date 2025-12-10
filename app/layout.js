b006cd1bd47006250b0d61b055fc2f669a6d8caa
dikorandiko8 <dikorandiko8@gmail.com>
2025-12-11 04:51:52 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,20 @@
 
+export const metadata = {
+  title: "Neynar Score Checker",
+  other: {
+    "fc:miniapp": JSON.stringify({
+      version: "1",
+      name: "Cek Neynar Score",
+      button: { title: "Cek Skor" },
+      iconUrl: "https://i.imgur.com/RAjFzQX.png"
+    })
+  }
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html>
+      <body>{children}</body>
+    </html>
+  );
+}
